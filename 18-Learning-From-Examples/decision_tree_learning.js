6d3bc79c1e35a912a03aff3ba7ff6f10e28c00a5
preethi <preethi.y14@iiits.in>
2017-03-26 15:49:25 +0530

    added decision tree learning implementation

---
+++ 18-Learning-From-Examples/decision_tree_learning.js
@@ -0,0 +1,238 @@
+
+// send the dataset from c_DecisionTreeLearning
+var Fork = function(attr,attrname,examples){
+	this.attr = attr;
+	this.examples = examples;
+	this.attrname = attrname;
+	this.branches= {};
+	this.add = function(value,subtree){
+		console.log(typeof(value));
+	this.branches[value] = subtree;
+	}
+
+}
+
+var Leaf = function(result,examples){
+	this.result = result;
+	this.examples = examples;
+}
+
+
+var dataset = {
+
+examples : [ [1,1,0,0,1,2,3,0,1,2,1] , [0,1,0,0,1,3,1,0,0,4,3] , [1,0,1,0,0,2,1,0,0,1,1] , [1,1,0,1,1,3,1,1,0,4,2] , [0,1,0,1,0,3,3,0,1,2,4] , [1,0,1,0,1,2,2,1,1,3,1] , [0,0,1,0,0,1,1,1,0,1,1] , [1,0,0,0,1,2,2,1,1,4,1] , [0,0,1,1,0,3,1,1,0,1,4] , [0,1,1,1,1,3,3,0,1,3,2] , [0,0,0,0,0,1,1,0,0,4,1] , [1,1,1,1,1,3,1,0,0,1,4]  ],
+attrs : [0,1,2,3,4,5,6,7,8,9,10],
+attributes : ["target", "one", "two","three","four","five","six","seven","eight","nine","ten"],
+target : 0 ,
+values : [[0,1],[0,1],[0,1],[0,1],[0,1],[1,2,3],[1,2,3],[0,1],[0,1],[1,2,3,4], [1,2,3,4]],
+
+}
+
+var DecisionTreeLearner = function(dataset){
+
+
+    this.dataset = dataset;
+    this.examples = dataset.examples;
+    this.attrs = dataset.attrs;
+    this.attributes = dataset.attributes;
+    this.target = dataset.target;
+    this.values = dataset.values;
+    this.target = dataset.target;
+    this.parent_examples = [];
+
+   function  DecisionTreeLearning(examples,attrs,parent_examples){
+    	if(examples.length===0){
+    		return plurality_value(parent_examples)
+    	} 
+    	else if( all_same_class(examples) ){
+    		return new Leaf(examples[0][target],examples)
+    	}
+    	else if(attrs.length===0){
+    		return plurality_value(examples)
+    	}
+    	else{
+    		var A = Importance(attrs,examples);
+    		console.log("attribute" + A+ "examples left" + examples.length);
+    		A= parseInt(A);
+    		var tree = new Fork(A,attributes[A],examples);
+    		var example_sets = values_set(A,examples);
+    		for ( var value in example_sets ) {
+  				if (example_sets.hasOwnProperty(value)) {
+
+    				var subtree = DecisionTreeLearning(example_sets[value] , remove_all(A,attrs),examples);
+
+    				tree.add(parseInt(value),subtree);
+    				 console.log(tree);
+    			}
+    		}
+
+    		return tree;
+    	}
+	}
+    
+    function plurality_value(examples){
+      var maximum =0;
+      var list =[];
+      var example_sets = values_set(target,examples);
+      for (var key in example_sets) {
+  			if (example_sets.hasOwnProperty(key)) {
+  				if(example_sets[key].length >  maximum ){
+      				maximum = example_sets[key].length;
+      				list=[];
+      				list.push(key);
+      			}
+      			else if (example_sets[key] === maximum) {
+      				list.push(key);
+      			}
+
+  			}
+		}
+
+      var popular = list[getRandomInt(0,list.length)];
+      return Leaf(Object.keys(popular)[0],popular[0]);
+
+
+    }
+
+    function getRandomInt(min, max) {
+    	max=max-1;
+    	return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+    function Importance(attrs,examples){
+    	var obj = {};
+    	for(var i=0;i<attrs.length;i++){
+    		if (attrs[i]!= 0){
+    		obj[attrs[i]] = info_gain(attrs[i],examples) ;
+    		}
+    	}
+    	console.log(obj);
+    	var maximum= 0;
+    	var list=[];
+    	for (var key in obj) {
+  			if (obj.hasOwnProperty(key)) {
+  				if(obj[key] >  maximum ){
+      				maximum = obj[key];
+      				list=[];
+      				list.push(key);
+      			}
+      			else if (obj[key] === maximum) {
+      				list.push(key);
+      			}
+
+  			}
+		}
+		var important = list[getRandomInt(0,list.length)];
+        return important;
+    }
+
+
+    function count(attr, val ,examples){
+   		var count =0;
+    	for( var i=0;i<examples.length;i++){
+    		if(examples[i][attr]===val){
+    			count=count+1;
+    		}
+    	}
+    	return count;
+
+    }
+
+    function all_same_class(examples){
+    	var class0 = examples[0][target];
+    	for (var i = 0; i < examples.length; i++) {
+    		if(examples[i][target] !== class0){
+    			return false;
+    		}
+    	}	
+    	return true;    
+    }
+
+
+    function remove_all(value,list){
+    	for(var i=0 ; i< list.length ; i++){
+    		if(list[i]===value){
+    			list.splice(i,1)
+    		}
+    	}
+    	return list;
+    }
+
+
+    function normalize(list){
+    	var total=0;
+    	for(var i =0;i<list.length;i++){
+    		total=total+ parseInt(list[i]);
+    	}
+    	for(var i=0;i<list.length;i++){
+    		list[i]= parseInt(list[i])/total;
+    	}
+    	return list;
+    }
+
+
+	function information_content(val){
+    	 var probabilities = normalize(remove_all(0,val))
+    	 var sum = 0;
+    	 for(var i=0;i < probabilities.length ; i++){
+    	 	var p=probabilities[i];
+    	 	sum = sum + ((-p)* (Math.log(p)/Math.log(2)));
+    	 }
+    	 return sum;
+    	}    
+
+
+    function info_gain(attr , examples){
+    	function bool_random_entropy(examples){
+    		var list = [];
+    		for ( var i=0;i<values[target].length;i++){
+    			list.push(count(target , values[target][i] , examples));
+    		}
+    		return information_content(list);
+    	}
+    	var remainder = function(){
+    		var n = examples.length;
+    		var sum =0;
+    		var example_sets =  values_set(attr,examples);
+    		for (var key in example_sets) {
+  				if (example_sets.hasOwnProperty(key)) {
+      			}	
+  			}
+    		for (var key in example_sets) {
+  				if (example_sets.hasOwnProperty(key)) {
+  					if (example_sets[key].length!==0) {
+	    				sum =sum + ((example_sets[key].length/ n) * bool_random_entropy(example_sets[key]));
+    				}
+  				}
+			}
+			return sum;
+
+    	}
+    	return bool_random_entropy(examples) - remainder();
+    }
+
+
+
+
+    function values_set(attr,examples){
+    	var obj={};
+    	console.log(attr + values[attr]);
+    	for( var i=0;i< values[attr].length;i++){
+    		var value = values[attr][i];
+    		var set = []
+    		for( var j=0;j<examples.length;j++){
+    			if(examples[j][attr]=== value){
+    				set.push(examples[j]);
+    			}
+    		}
+    		obj[value]=set;
+    	}
+    	return obj;
+    }
+
+    return DecisionTreeLearning(examples,attrs,parent_examples)
+
+}
+
+
+
